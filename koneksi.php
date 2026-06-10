5ba0ea23d7afdcefc199f6c1fe5fb862c0e0902d
aurelputrii125-cmd <aurelputrii125@gmail.com>
2026-06-11 00:03:01 +0700

    perbarui

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,16 @@
 <?php
-$host = "localhost"; 
-$user = "root";      
-$pass = "";          
-$db   = "outdoor_db";
+
+$host = "sql208.infinityfree.com";
+$user = "if0_42149711";
+$pass = "Matchaw01";
+$db   = "if0_42149711_outdoor_db";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+echo "Koneksi database berhasil!";
+
 ?>
